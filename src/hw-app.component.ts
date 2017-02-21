09317df379605362dbdc2fc6f023d85f8549836d
prajapatkiran <prajapatkiran@gmail.com>
2017-02-21 16:30:36 -0500

    Changed dependency version for angular.

--- src/hw-app.component.ts
+++ src/hw-app.component.ts
@@ -13,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
     <a routerLink="/kee-hw-app/hello-world-sub" routerLinkActive="active">Kee Hello World -:Sub:- Route!</a>
   </nav>
   <router-outlet></router-outlet>`,
-  styles: [``]
+  styles: []
 })
 export class HwAppComponent implements OnInit {
 
