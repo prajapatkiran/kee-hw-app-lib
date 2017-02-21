96f42b5f5bb7a5740be1152a58e4857e3bd0910c
prajapatkiran <prajapatkiran@gmail.com>
2017-02-21 15:41:08 -0500

    Inlined html & css

--- src/hw-app.component.ts
+++ src/hw-app.component.ts
@@ -2,8 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'kee-hw-app',
-  templateUrl: './hw-app.component.html',
-  styleUrls: ['./hw-app.component.css']
+  template: `<p>
+    hw-app works!
+    <kee-app-abcd></kee-app-abcd>
+  </p>
+  <nav>
+    <a routerLink="/kee-hw-app/hello-world" routerLinkActive="active">Kee Hello World Route!</a>
+  </nav>
+  <nav>
+    <a routerLink="/kee-hw-app/hello-world-sub" routerLinkActive="active">Kee Hello World -:Sub:- Route!</a>
+  </nav>
+  <router-outlet></router-outlet>`,
+  styles: [``]
 })
 export class HwAppComponent implements OnInit {
 
