e19e3f50fa313e7556cacd8c753f3541245c0abc
prajapatkiran <prajapatkiran@gmail.com>
2017-02-22 10:50:11 -0500

    Udates to make it a working third party module.

--- index.d.ts
+++
@@ -1,1 +0,0 @@
-export * from "./lib/kee-hw-app-module";
---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * @module
+ * @description
+ * API exported by this module.
+ */
+export * from './lib/index';
+
+// This file only reexports content of the public_api. Keep it that way.
